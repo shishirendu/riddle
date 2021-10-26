443645bb471234ef132f89324f89b2bbf287948f
shishirendu <shishirendu@gmail.com>
2021-10-26 10:39:14 +0800

    setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='riddle',
+      version='0.0.1',
+      description='The toughest riddles in the world',
+      url='https://github.com/shishirendu/riddle',
+      author='Shishir',
+      author_email='shishirendu@gmail.com',
+      license='MIT',
+      packages=['riddle'],
+      zip_safe=False)
